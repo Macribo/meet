c074402708bbe52633d44de200df52fabc1e06ac
ribo <roibeard.mac.unfraidh@gmail.com>
2020-12-28 21:20:26 +0100

    Updates

---
+++ precache-manifest.942b4c35570c728682860731a83a569e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65b70e21af38c830a83c89ac5cd9727",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "262586e2312499433671",
+    "url": "/meetup/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d836c5b05cb89eeaed4",
+    "url": "/meetup/static/js/2.783d8e07.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/meetup/static/js/2.783d8e07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "262586e2312499433671",
+    "url": "/meetup/static/js/main.5330fb20.chunk.js"
+  },
+  {
+    "revision": "5b15bedc16c52474edcc",
+    "url": "/meetup/static/js/runtime-main.61f01ad6.js"
+  }
+]);
